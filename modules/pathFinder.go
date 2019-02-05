8ff315a1c42f206fbc47e7d49e671847d41f3062
Reidmcc <reid.mccamish@gmail.com>
2019-02-05 00:54:19 -0600

    make path-requesting a separate object

--- modules/pathFinder.go
+++ modules/pathFinder.go
@@ -115,7 +115,6 @@ type TradingPair struct {
 }
 
 // PaymentPath is a pair of assets for a payment path and their encoded tradingPair
-// probably don't need separate PriceFeed concepts if the tradingPair is here
 type PaymentPath struct {
 	HoldAsset  horizon.Asset
 	PathAssetA horizon.Asset
@@ -203,8 +202,8 @@ func (p *PathFinder) FindBestPath() (*PaymentPath, *model.Number, bool, error) {
 
 // calculatePathValues returns the path's best ratio and max amount at that ratio
 func (p *PathFinder) calculatePathValues(path PaymentPath) (*model.Number, *model.Number, error) {
-	// first pair is buying asset A with the hold asset
-	// switch to selling direction, which means switch first pair factory order
+
+	// first pair is selling the hold asset for asset A
 	firstPairTopBidPrice, firstPairTopBidAmount, e := p.dexWatcher.GetTopBid(path.FirstPair)
 	if e != nil {
 		return nil, nil, fmt.Errorf("Error while calculating path ratio %s", e)
@@ -265,199 +264,6 @@ func (p *PathFinder) calculatePathValues(path PaymentPath) (*model.Number, *mode
 	}
 
 	return ratio, maxCycleAmount, nil
-}
-
-// AnalysePaths asks horizon about available paths and checks if any are profitable
-func (p *PathFinder) AnalysePaths() (*PathRecord, *model.Number, bool, error) {
-	metThreshold := false
-	var bestPath PathRecord
-	var bestCost *model.Number
-	//var amount *model.Number
-	var pathOutput *model.Number
-
-	for i := p.assetBookMark; i < len(p.assetBook); i++ {
-		currentAsset := p.assetBook[i]
-		p.assetBookMark++
-		if p.assetBookMark >= len(p.assetBook) {
-			p.assetBookMark = 0
-		}
-		pair := TradingPair{
-			Base:  currentAsset.Asset,
-			Quote: p.HoldAsset,
-		}
-
-		bidPrice, _, e := p.dexWatcher.GetTopBid(pair)
-		if e != nil {
-			return nil, nil, false, fmt.Errorf("Error while analysing paths %s", e)
-		}
-
-		amountConversion := model.NumberConstants.One.Divide(*bidPrice)
-		// if we want to keep useBalance we have to put a balance call here
-		minConvertedAmount := amountConversion.Multiply(*p.staticAmount)
-		p.l.Info("")
-		// p.l.Infof("Amount ratio calced at %v\n", amountConversion.AsFloat())
-		// p.l.Infof("Converted minAmount calced at %v\n", minConvertedAmount.AsFloat())
-		// p.l.Info("")
-
-		// now find paths where the desired amount goes through
-		pathData, e := p.dexWatcher.GetPaths(currentAsset.Asset, minConvertedAmount)
-		if e != nil {
-			return nil, nil, false, fmt.Errorf("error while analysing paths %s", e)
-		}
-
-		var intermediatePaths []PathRecord
-
-		for _, s := range pathData.Embedded.Records {
-			//p.l.Infof("checking path %v+, witch has a path struct of length %v", s, len(s.Path))
-			if len(s.Path) > 0 {
-				//p.l.Info("added the checked path to the candidate list")
-				intermediatePaths = append(intermediatePaths, s)
-			}
-		}
-
-		if len(intermediatePaths) == 0 {
-			p.l.Infof("No paths with intermediaries found for %s|%s", currentAsset.Asset.Code, currentAsset.Asset.Issuer)
-			continue
-		}
-
-		//p.l.Infof("Found intermediate paths: %+v", intermediatePaths)
-
-		for i := 0; i < len(intermediatePaths); i++ {
-			if i == 0 {
-				bestPath = intermediatePaths[i]
-				bestCost, e = model.NumberFromString(intermediatePaths[i].SourceAmount, utils.SdexPrecision)
-				if e != nil {
-					return nil, nil, false, fmt.Errorf("error while analysing paths %s", e)
-				}
-				continue
-			}
-
-			pathCost, e := model.NumberFromString(intermediatePaths[i].SourceAmount, utils.SdexPrecision)
-			if e != nil {
-				return nil, nil, false, fmt.Errorf("error while analysing paths %s", e)
-			}
-
-			if pathCost.AsFloat() < bestCost.AsFloat() {
-				bestPath = intermediatePaths[i]
-				bestCost = pathCost
-			}
-		}
-
-		// now see if output is greater than input; if not go to the next asset
-		destAmount, e := model.NumberFromString(bestPath.DestinationAmount, utils.SdexPrecision)
-		if e != nil {
-			return nil, nil, false, fmt.Errorf("error while analysing paths %s", e)
-		}
-		pathOutput := destAmount.Multiply(*bidPrice)
-		pathRatio := pathOutput.Divide(*bestCost)
-		p.l.Infof("Best path amount output for %s|%s was %v for %v", currentAsset.Asset.Code, currentAsset.Asset.Issuer, pathOutput.AsFloat(), bestCost.AsFloat())
-		p.l.Infof("With a ratio if %v", pathRatio.AsFloat())
-		//p.l.Infof("Raw best cost was %v ; raw converted amount was was %v", bestCost.AsFloat(), minConvertedAmount.AsFloat())
-
-		if pathOutput.Scale(1.0/p.minRatio.AsFloat()).AsFloat() > bestCost.AsFloat() && len(bestPath.Path) > 0 {
-			metThreshold = true
-			p.l.Info("")
-			p.l.Info("***** Minimum profit ratio was met, proceeding to payment! *****")
-			p.l.Info("")
-			// trying a method that doesn't analyst path throughput, just uses bestCost
-			// should be faster and more accurate
-			//amount, e := p.findMaxAmount(bestPath)
-			if e != nil {
-				return nil, nil, false, fmt.Errorf("error while analysing paths %s", e)
-			}
-			return &bestPath, pathOutput, metThreshold, nil
-		}
-
-	}
-	return &bestPath, pathOutput, false, nil
-}
-
-// findMaxAmount finds the amount for the AnalysePaths functions
-// this likely needs math edits due to bid amount inversion
-func (p *PathFinder) findMaxAmount(sendPath PathRecord) (*model.Number, error) {
-	var pathPairs []TradingPair
-	var pair TradingPair
-
-	for i := 0; i < len(sendPath.Path); i++ {
-		if i == 0 {
-			pair = TradingPair{
-				Base:  p.HoldAsset,
-				Quote: PathAsset2Asset(sendPath.Path[i]),
-			}
-			continue
-		}
-
-		pair = TradingPair{
-			Base:  PathAsset2Asset(sendPath.Path[i-1]),
-			Quote: PathAsset2Asset(sendPath.Path[i]),
-		}
-
-		pathPairs = append(pathPairs, pair)
-	}
-
-	destAsset := ParseAsset(sendPath.DestinationAssetCode, sendPath.DestinationAssetIssuer)
-
-	lastPair := TradingPair{
-		Base:  PathAsset2Asset(sendPath.Path[len(sendPath.Path)-1]),
-		Quote: destAsset,
-	}
-
-	postPair := TradingPair{
-		Base:  destAsset,
-		Quote: p.HoldAsset,
-	}
-
-	pathPairs = append(pathPairs, lastPair)
-	pathPairs = append(pathPairs, postPair)
-
-	for _, d := range pathPairs {
-		p.l.Infof("found path asset: %s", d)
-	}
-
-	var bidSeries []basicOrderBookLevel
-
-	for _, r := range pathPairs {
-		topBidPrice, topBidAmount, e := p.dexWatcher.GetTopBid(r)
-		if e != nil {
-			return nil, fmt.Errorf("Error while calculating path amount %s", e)
-		}
-		bidSeries = append(
-			bidSeries,
-			basicOrderBookLevel{
-				Price:  topBidPrice,
-				Amount: topBidAmount,
-			},
-		)
-	}
-
-	if len(bidSeries) <= 1 {
-		return nil, fmt.Errorf("not enough pairs in path, or failed get pair orderbooks")
-	}
-
-	p.l.Infof("generated bidSeries of %v+", bidSeries)
-
-	maxInput := bidSeries[0].Amount
-	inAmount := bidSeries[0].Amount.Multiply(*bidSeries[0].Price)
-
-	for i := 1; i < len(bidSeries); i++ {
-		outAmount := bidSeries[i].Amount
-		if inAmount.AsFloat() < outAmount.AsFloat() {
-			outAmount = inAmount
-		}
-		inAmount = outAmount.Multiply(*bidSeries[i].Price)
-	}
-
-	maxOutput := inAmount
-
-	if maxInput.AsFloat() < maxOutput.AsFloat() {
-		maxOutput = maxInput
-	}
-
-	if p.useBalance == false && p.staticAmount.AsFloat() < maxOutput.AsFloat() {
-		maxOutput = p.staticAmount
-	}
-
-	return maxOutput, nil
 }
 
 // WhatRatio returns the minimum ratio
